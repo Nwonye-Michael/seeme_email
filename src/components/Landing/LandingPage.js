90828050297cc51ced7debc8f133f596869157ab
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-02-28 13:33:19 +0100

    Update LandingPage.js

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -5,7 +5,22 @@ import Overlay from "../Overlay/Overlay"
 import "./Landing.css"
 
 function LandingPage() {
+  const [phoneWidth, setPhoneWidth] = useState(false)
+  const [pcWidth, setPcWidth] = useState(false)
   const [showModal, setShowModal] = useState()
+  const { innerWidth: width } = window
+  useEffect(() => {
+    if (window.innerWidth <= 480) {
+      setPhoneWidth(true)
+      setPcWidth(false)
+      console.log(window, width, "mobile")
+    } else if (window.innerWidth > 480) {
+      setPcWidth(true)
+      setPhoneWidth(false)
+      console.log(window, width, "pc")
+    }
+  }, [width])
+
   return (
     <div className="container">
       <div className="landingpage">
@@ -16,18 +31,23 @@ function LandingPage() {
               <span>Keep track of your</span>
               <span className="time__span">
                 <span> loved ones in real </span>
-                <span>time!</span>{" "}
+                <span>time!</span>
               </span>
             </div>
-            <div className="content__desc">
-              <span>
-                We are building a one click <>tracking app for your</>
-              </span>
-              <span>
-                <>friends </>and families.
-              </span>
-            </div>
-
+            {pcWidth && (
+              <div className="content__desc">
+                <span>We are building a one click tracking app for your</span>{" "}
+                <span>friends and families.</span>
+                <span></span>
+              </div>
+            )}{" "}
+            {phoneWidth && (
+              <div className="content__desc content__desc__mobile">
+                <div>We are building a one click</div>
+                <div>tracking app for your friends</div>
+                <div>and families.</div>
+              </div>
+            )}
             <form
               className="form"
               onSubmit={(e) => {
@@ -52,3 +72,4 @@ function LandingPage() {
 }
 
 export default LandingPage
+
